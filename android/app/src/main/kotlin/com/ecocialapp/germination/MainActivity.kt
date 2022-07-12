f55bf1bb57cfbe9c745e8005182b17d7d28da586
Anjani Mirchandani <ajm.orion@gmail.com>
2022-07-12 15:21:52 -0700

    changed package name

--- android/app/src/main/kotlin/com/ecocialapp/germination/MainActivity.kt
+++ android/app/src/main/kotlin/com/ecocialapp/germination/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.project_ecocial
+package com.ecocialapp.germination
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -140,7 +140,7 @@ class _CommentsScreenState extends State<CommentsScreen> {
                     postCommentWidget,
                     noCommentsWidget,
                   ],
-                );
+                 );
               }
 
             },
